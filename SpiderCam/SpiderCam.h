a0ef01a054ea64731bf6ebd01105d7e785ff1c6e
luis <luis.nickel@hotmail.de>
2021-06-21 23:35:59 +0200

    init

---
+++ SpiderCam/SpiderCam.h
@@ -0,0 +1,22 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _SpiderCam_H_
+#define _SpiderCam_H_
+#include "Arduino.h"
+//add your includes for the project SpiderCam here
+
+
+//end of add your includes here
+
+
+//add your function definitions for the project SpiderCam here
+
+
+
+
+//Do not add code below this line
+#endif /* _SpiderCam_H_ */
